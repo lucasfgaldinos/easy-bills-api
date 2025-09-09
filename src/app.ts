d0dfcac32e53c65b3425a720c16217b2def3942c
lucasfgaldinos <lucasfgaldinos@outlook.com>
2025-09-09 16:07:01 -0300

    chore: configurar fastify/cors

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import cors from "@fastify/cors";
 import type { FastifyInstance } from "fastify";
 import fastify from "fastify";
 import { env } from "./config/env.config";
@@ -8,5 +9,7 @@ export const app: FastifyInstance = fastify({
 		level: env.NODE_ENV === "dev" ? "info" : "error",
 	},
 });
+
+app.register(cors);
 
 app.register(routes, { prefix: "api" });
